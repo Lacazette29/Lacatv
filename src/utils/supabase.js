79807f2f4009a6cd15960ba61a323e2ab215add9
Afrivahub <afrivahub@gmail.com>
2026-04-21 09:11:35 -0700

    Fix src/utils/supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-=======
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl  = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey  = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
->>>>>>> 818a1abf7d4f49b0e6ba7320705f9a4bbc6182a9
